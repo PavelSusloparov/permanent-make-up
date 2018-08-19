6cd7dc772c2d62d854cbd39c892857c92851eec3
Pavel Susloparov <pavel.susloparov@nytimes.com>
2018-08-18 22:54:37 -0400

    Add flyway for backend-service. Move 1st table creation from docker-entrypoint to .sql. Remove target folders for all services.

--- docker-entrypoint-initdb.d/startup.sql
+++ docker-entrypoint-initdb.d/startup.sql
@@ -4,11 +4,3 @@ CREATE USER 'permanent_make_up_rwx'@'%';
 CREATE DATABASE IF NOT EXISTS main;
 GRANT SELECT, INSERT, DELETE, UPDATE ON main.* TO 'permanent_make_up_rwx'@'%' IDENTIFIED BY 'ekl84';
 GRANT ALL ON main.* TO 'permanent_make_up_rwx'@'%' IDENTIFIED BY 'ekl84';
-
-CREATE TABLE main.guestbook_message (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  name CHAR(128) NOT NULL,
-  message CHAR(255),
-  image_uri CHAR(255),
-  PRIMARY KEY (id)
-);
