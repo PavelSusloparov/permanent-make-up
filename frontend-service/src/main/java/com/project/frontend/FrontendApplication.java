cd11f7198af059df3e06b3249027514685be66a7
Pavel Susloparov <pavel.susloparov@nytimes.com>
2018-08-20 00:13:00 -0400

    Add gitignore. Expose endpoints for backend for showing information in admin. Create local profile for frontend service.

--- frontend-service/src/main/java/com/project/frontend/FrontendApplication.java
+++ frontend-service/src/main/java/com/project/frontend/FrontendApplication.java
@@ -6,12 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 
-@SpringBootApplication
-// Enable consumption of HATEOS payloads
-@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
-// Enable Feign Clients
 @EnableFeignClients
 @EnableDiscoveryClient
+@SpringBootApplication
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class FrontendApplication {
 
 	public static void main(String[] args) {
