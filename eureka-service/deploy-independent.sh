d2fcbf48bc62417d903dceb56879cd65a38f231d
Pavel Susloparov <pavel.susloparov@nytimes.com>
2018-08-27 01:10:10 -0400

    kubernetes deployment.wip.admin is ready.backend i hate u

--- eureka-service/deploy-independent.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl apply -f ../kubernetes/k8s-eureka.yml
--- frontend-service/deploy.sh
+++ frontend-service/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./mvnw appengine:deploy -Dspring.profiles.active=cloud -DskipTests
+kubectl apply -f ../kubernetes/frontend-service-deployment.yml
