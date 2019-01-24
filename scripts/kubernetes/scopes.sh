8b7134813da62f06b09b748f6abf20f1938a6163
PavelSusloparovNYT <pavel.susloparov@nytimes.com>
2019-01-24 13:19:38 -0500

    Add scopes file

---
+++ scripts/kubernetes/scopes.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# shellcheck disable=SC2034
+SCOPES="https://www.googleapis.com/auth/bigquery
+https://www.googleapis.com/auth/cloud-platform
+https://www.googleapis.com/auth/cloud.useraccounts
+https://www.googleapis.com/auth/cloud.useraccounts.readonly
+https://www.googleapis.com/auth/compute
+https://www.googleapis.com/auth/compute.readonly
+https://www.googleapis.com/auth/datastore
+https://www.googleapis.com/auth/devstorage.full_control
+https://www.googleapis.com/auth/devstorage.read_only
+https://www.googleapis.com/auth/devstorage.read_write
+https://www.googleapis.com/auth/logging.write
+https://www.googleapis.com/auth/monitoring
+https://www.googleapis.com/auth/monitoring.write
+https://www.googleapis.com/auth/pubsub
+https://www.googleapis.com/auth/service.management.readonly
+https://www.googleapis.com/auth/servicecontrol
+https://www.googleapis.com/auth/source.full_control
+https://www.googleapis.com/auth/source.read_only
+https://www.googleapis.com/auth/sqlservice
+https://www.googleapis.com/auth/sqlservice.admin
+https://www.googleapis.com/auth/taskqueue
+https://www.googleapis.com/auth/trace.append
+https://www.googleapis.com/auth/userinfo.email"
